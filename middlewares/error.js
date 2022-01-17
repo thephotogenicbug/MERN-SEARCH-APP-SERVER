a14c2a676fcb06b06c07e02cb30f2dec7f8058f4
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-17 17:52:01 +0530

    Handler more Errors

--- middlewares/error.js
+++ middlewares/error.js
@@ -4,8 +4,14 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal server error";
 
+  // wrong Mongodb id error
+  if (err.name === "CastError") {
+    const message = `Resource not found invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
+    // message: err.stack,
   });
 };
--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const app = require("./app");
+
 const dotenv = require("dotenv");
 const connectDB = require("./config/database");
 
+// Handling uncaught exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to uncaught exception`);
+  process.exit(1);
+});
+
+//config
 dotenv.config({ path: "config/config.env" });
+
+// connecting to database
 connectDB();
 
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
 
+// unhandled Promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to unhandled promise rejection`);
 
-app.listen(process.env.PORT, () => {
-  console.log(`server is running on port http://localhost:${process.env.PORT}`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
