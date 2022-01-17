c94591ce36e3c56589468a8c8be4f7238f1797fa
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-17 17:43:15 +0530

    Build ErrorMiddleware

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const errorMiddleware = require("./middlewares/error");
 
 app.use(express.json());
 
-const course = require('./routes/courseRoute')
-app.use('/api/v1',course )
+const course = require("./routes/courseRoute");
+app.use("/api/v1", course);
+
+// Error middleware
+app.use(errorMiddleware);
 
 module.exports = app;
