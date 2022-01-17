2dbe279bbca4df153f1fc45d5a7dd3276c9ca657
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-17 14:39:55 +0530

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const app = require('./app')
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDB = require("./config/database");
+
+dotenv.config({ path: "config/config.env" });
+connectDB();
 
 
 
-app.listen(process.env.PORT, () =>{
-    console.log(
-      `server is running on port http://localhost:${process.env.PORT}`
-    );
-})
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port http://localhost:${process.env.PORT}`);
+});
