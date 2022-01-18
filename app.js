1e9fed3bb085754810fb6453b9b90dacc3079449
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-18 16:00:03 +0530

    Enable cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const errorMiddleware = require("./middlewares/error");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 const course = require("./routes/courseRoute");
 app.use("/api/v1", course);
